7e2e23455767d354539bbb1fd87abe0f16381494
eryk <eryk.czyzak@gmail.com>
2024-01-21 19:26:14 +0100

    fixed bug: water can destroy surface ores

--- src/main/java/com/thecode/createdeposits/block/SurfaceOreBlock.java
+++ src/main/java/com/thecode/createdeposits/block/SurfaceOreBlock.java
@@ -6,15 +6,32 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class SurfaceOreBlock extends Block {
+public class SurfaceOreBlock extends Block implements SimpleWaterloggedBlock {
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
     public SurfaceOreBlock() {
         super(Properties.copy(Blocks.IRON_ORE).strength(5.0f, 6.0f).noOcclusion().pushReaction(PushReaction.BLOCK).requiresCorrectToolForDrops());
+        this.registerDefaultState(defaultBlockState().setValue(WATERLOGGED, false));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(WATERLOGGED);
+    }
+
+    public FluidState getFluidState(BlockState pState) {
+        return pState.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(pState);
     }
 
     @Override
